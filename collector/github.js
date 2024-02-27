c884fb56d87167b3240b379e00dbcab6c969c1b3
Parzival <malte.linke@outlook.de>
2024-02-27 14:37:19 +0100

    fix version check

--- collector/github.js
+++ collector/github.js
@@ -13,8 +13,8 @@ const latestVersion = releaseResp.data.tag_name.replace('v', '');
 const installerUrl = releaseResp.data.assets.find(a => a.name === `goxlr-utility-${latestVersion}.exe`)?.browser_download_url;
 
 // check if version is up-to-date
-const wingetResp = await client.get(`/repos/microsoft/winget-pkgs/contents/manifests/g/GoXLR-on-Linux/GoXLR-Utility/v${latestVersion}`);
-const isPublished = wingetResp.status !== 404;
+const wingetResp = await client.get(`/repos/microsoft/winget-pkgs/contents/manifests/g/GoXLR-on-Linux/GoXLR-Utility/v${latestVersion}`).catch(() => null);
+const isPublished = wingetResp === null ? false : wingetResp.status !== 404;
 
 // export results for actions workflow
 core.setOutput('latestVersion', latestVersion);
